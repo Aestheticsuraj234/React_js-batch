913b5a7f75773e0c369054225602fda574fddbe0
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-04 20:37:48 +0530

    Fix bug in login functionality

---
+++ 04_Basics_Javascript/main.js
@@ -0,0 +1,51 @@
+// let arr = [1, 2, 3, 4, 5]
+
+// // let fruits = ["Apple", "Banana", "Orange"];
+
+// // console.log(fruits[0]);
+// // console.log(fruits[1]);
+// // console.log(fruits[2]);
+
+
+// let userDatas =
+//     [
+//         {
+//             name: "John",
+//             age: 30
+
+//         }
+//         ,
+//         {
+//             name: "Smith",
+//             age: 40
+
+//         }
+//         ,
+//         {
+//             name: "Doe",
+//             age: 50
+//         }
+
+//     ]
+
+
+// console.log(userDatas[0]);
+
+
+
+// let  fruits = ["Apple", "Banana", "Orange"];
+
+// //* push() method
+// // fruits.push("Mango");
+
+// // console.log(fruits);
+
+// let lastFruit = fruits.pop(); // Removes "Mango" from the end
+// console.log(lastFruit); // Output: "Mango"
+// console.log(fruits); // Output: ["Apple", "Grapes", "Orange"]
+
+let colors = ["Red", "Green", "Blue"];  //✅
+
+let numbers = new Array(1, 2, 3, 4, 5)
+
+console.log(numbers)
