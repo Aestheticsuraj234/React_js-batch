dbb424619561de9354251bcdc504fc97fb333cf3
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-11 15:23:00 +0530

    String_Method

---
+++ 08_Basics_javascript/main.js
@@ -0,0 +1,82 @@
+let SampleString = "Hello World!";
+
+//* 1 Length- Property
+console.log(SampleString.length); // 12
+
+// *2. ToUpperCase()
+
+let values = "my name is suraj";
+let val = values.toUpperCase()
+console.log(val)
+
+// *3. ToLowerCase()
+let values1 = "MY NAME IS SURAJ";
+let val1 = values1.toLowerCase()
+console.log(val1)
+
+// *4. charAt()
+let str = "Hello World!";
+let res = str.charAt(12);
+console.log(res);
+
+// *5. indexOf()
+let str1 = "Hello World!";
+let res1 = str1.indexOf("l");
+console.log(res1);
+
+// *6. lastIndexOf()
+
+let str2 = "Hello World!";
+let res2 = str2.lastIndexOf("l");
+console.log(res2);
+
+
+// *7. includes()
+let str3 = "Hello World!";
+let res3 = str3.includes("World");
+console.log(res3);
+
+// *8. replace()
+let str4 = "Hello World!";
+let res4 = str4.replace("World", "Suraj");
+console.log(res4);
+
+
+// *9. concat()
+let str5 = "Hello";
+let res5 = str5.concat(" World!");
+console.log(res5);
+
+// *10. slice()
+let str6 = "Hello World!";
+let res6 = str6.slice(6, 12);
+console.log(res6);
+
+// *11. substr:
+let str7 = "Hello World!";
+let res7 = str7.substr(6, 2);
+console.log(res7);
+
+
+// * replace()
+let str8 = "Hello World!";
+let res8 = str8.replace("World", "Suraj");
+console.log(res8);
+
+
+// Write Reduce function without using reduce method
+
+function reduce(arr, cb, acc) {
+    for (let i = 0; i < arr.length; i++) {
+        acc = cb(acc, arr[i]);
+    }
+    return acc;
+}
+
+
+
+let arr = [1, 2, 3, 4, 5];
+let sum = reduce(arr, (acc, val) => acc + val, 0);
+
+console.log(sum); // 15
+
