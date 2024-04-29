0622bae98ffa2600f7b567d82b39fe23eb9a3a6f
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-29 15:37:20 +0530

    day-17

---
+++ 17_Advance_javascript/main.js
@@ -0,0 +1,37 @@
+// // // *sync and async?
+
+// // console.log("I am a sync code") 
+// // setTimeout(()=>{
+// //     console.log("I am a async code")
+// // } , 5000)
+// // c
+
+setInterval
+setTimeout
+fetch
+axios
+Promise
+async / await 
+ XMLHttpRequest
+
+
+
+// // // *asynx js?
+// // // *js is not async by default
+
+
+
+// // Sync
+// console.log("Hello-world_1")
+// console.log("Hello-world_2")
+// console.log("Hello-world_3")
+// console.log("Hello-world_4")
+// console.log("Hello-world_5")
+
+// // async
+
+// console.log("task_1")
+// setTimeout(()=>{
+//     console.log("task_2")
+// }, 2000)
+// console.log("task_3")
