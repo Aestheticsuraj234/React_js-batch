50e00c8ea9d7a4c15fa9dcf1f196f1ebc97ed31a
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-05-15 21:51:31 +0530

    useState

--- 20_React/my-app/src/App.js
+++ 20_React/my-app/src/App.js
@@ -1,14 +1,47 @@
+import React , {useState} from 'react'
 
 const App = () => {
+    // use State Hooks
+    const [count , setCount] = useState(0);
+    const [toggleMode , setToggleMode] = useState("dark");
+    const [isDark , setIsDark] = useState(true);
+    
+     const toggleModeHandler = ()=>{
+        if(isDark)
+            {
+                setToggleMode("light");
+                setIsDark(false);
+            }
+            else
+            {
+                setToggleMode("dark");
+                setIsDark(true);
+            }
+     }
 
 
+     console.log(toggleMode)
+    
     return (
-        <div className="bg-gray-800 h-screen w-full flex">
-        <h1 className="text-3xl text-yellow-600 font-bold">Hello world</h1>
+    
+    <div className={`h-screen flex justify-center items-center gap-6 ${(toggleMode==="dark" && isDark===true) ? "bg-gray-800 text-white": "bg-gray-100 text-black" }`}>
+                <button onClick={()=>setCount(count+1)} className='bg-green-500 px-4 py-2 rounded-md  font-semibold text-xl'>+</button>
+                <span className="text-xl font-bold ">{count}</span>
+                <button  onClick={()=>setCount(count-1)} className='bg-red-500 px-4 py-2 rounded-md  font-semibold text-xl'>-</button>
 
-        </div>
-    )
+                <button onClick={toggleModeHandler} className='bg-white px-4 py-2 rounded-md text-black font-semibold text-xl' >
+                    {(toggleMode==="dark" && isDark===true) ? "Light Mode" : "Dark Mode"  }
+                </button>
+    </div>
+  )
 }
 
+export default App
 
-export default App;
+
+    // Hook Rules
+    // There are 3 rules for hooks:
+    
+    // Hooks can only be called inside React function components.
+    // Hooks can only be called at the top level of a component.
+    // Hooks cannot be conditional
